a963d770181ebe1722afb2045dc9070cc596778b
Tomasz Dysinski <oz@bunnybones.com>
2014-10-09 14:41:10 -0400

    added min and max sizes. no dispatches for unchanged sizes

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 var EventUtils = require('browser-event-adder');
 var signals = require('signals');
 var onResize = new signals.Signal();
+
+var lastW = 0,
+	lastH = 0;
+
+var Resize = {
+	minHeight : 16,
+	maxHeight : 100000,
+	minWidth : 16,
+	maxWidth : 100000
+}
+
+function adjustedWidth() {
+	return Math.max(Resize.minWidth, Math.min(Resize.maxWidth, window.innerWidth));
+}
+
+function adjustedHeight() {
+	return Math.max(Resize.minHeight, Math.min(Resize.maxHeight, window.innerHeight));
+}
+
+function bump(resizeCallback) {
+	onResize.dispatch(
+		adjustedWidth(), 
+		adjustedHeight()
+	);
+}
+
 EventUtils.addEvent(window, "resize", function(event) {
-	onResize.dispatch(window.innerWidth, window.innerHeight);
+	var w = adjustedWidth();
+	var h = adjustedHeight();
+	if(w == lastW && h == lastH) return;
+	onResize.dispatch(w, h);
+	lastW = w;
+	lastH = h;
 });
-module.exports = {
-	onResize: onResize
-};
+
+Resize.onResize = onResize;
+Resize.bump = bump;
+
+module.exports = Resize;
--- test.js
+++ test.js
@@ -2,6 +2,9 @@ var ResizeManager = require('./');
 var label = document.createTextNode("Hello World");
 document.body.appendChild(label);
 
+ResizeManager.minWidth = 800;
+ResizeManager.minHeight = 600;
+
 ResizeManager.onResize.add(function(x, y) {
 	label.replaceWholeText(x + ' ' + y);
 });
